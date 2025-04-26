3579f75efddcf8cfda566c1ab372333ebcce65c0
Jiang Yiheng <jj11hh@live.com>
2025-04-26 23:04:18 +0800

    chore: migrate to bevy 0.16

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod game_state;
 mod terrain;
 
 use bevy::prelude::*;
+use bevy_inspector_egui::bevy_egui::EguiPlugin;
 use bevy_inspector_egui::quick::{ResourceInspectorPlugin, WorldInspectorPlugin};
 use movement::movement;
 use crate::terrain::{Terrain2dPlugin, TerrainChunkRenderers, TILE_PIXEL_SIZE};
@@ -18,10 +19,11 @@ fn main() {
             watch_for_changes_override: Some(true),
             ..Default::default()
         }),
-        WorldInspectorPlugin::new(),
         Terrain2dPlugin,
         GameStatePlugin,
+        EguiPlugin { enable_multipass_for_primary_context: false },
         ResourceInspectorPlugin::<TerrainChunkRenderers>::default(),
+        WorldInspectorPlugin::new(),
     ))
         .add_systems(Startup, startup)
         .add_systems(Update, movement)
@@ -33,9 +35,11 @@ fn startup(
 ) {
     let mut ortho = OrthographicProjection::default_2d();
     ortho.scale = 1.0 / TILE_PIXEL_SIZE;
+    
+    let projection = Projection::Orthographic(ortho);
 
     commands.spawn((
         Camera2d,
-        ortho
+        projection
     ));
 }
---
+++ src/movement.rs
@@ -0,0 +1,52 @@
+﻿use std::ops::DerefMut;
+use bevy::input::ButtonInput;
+use bevy::math::Vec3;
+use bevy::prelude::{Camera, KeyCode, Projection, Query, Res, Time, Transform, With};
+use crate::terrain::TILE_PIXEL_SIZE;
+
+pub fn movement(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut Projection), With<Camera>>,
+) {
+    for (mut transform, mut proj) in query.iter_mut() {
+        let ortho = match proj.deref_mut() {
+            Projection::Orthographic(proj) => proj,
+            _ => continue,
+        };
+
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyH) {
+            direction -= Vec3::new(1.0, 0.0, 0.0) / TILE_PIXEL_SIZE;
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyL) {
+            direction += Vec3::new(1.0, 0.0, 0.0) / TILE_PIXEL_SIZE;
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyK) {
+            direction += Vec3::new(0.0, 1.0, 0.0) / TILE_PIXEL_SIZE;
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyJ) {
+            direction -= Vec3::new(0.0, 1.0, 0.0) / TILE_PIXEL_SIZE;
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyZ) {
+            ortho.scale += 0.1 / TILE_PIXEL_SIZE;
+        }
+
+        if keyboard_input.pressed(KeyCode::KeyX) {
+            ortho.scale -= 0.1 / TILE_PIXEL_SIZE;
+        }
+
+        ortho.scale = ortho.scale.clamp(0.5 / TILE_PIXEL_SIZE, 5.0 / TILE_PIXEL_SIZE);
+
+        let z = transform.translation.z;
+        transform.translation += time.delta_secs() * direction * 500.;
+        // Important! We need to restore the Z values when moving the camera around.
+        // Bevy has a specific camera setup and this can mess with how our layers are shown.
+        transform.translation.z = z;
+    }
+}
